c3569e0a59cc188be9d03fee96a0f18f645bc49f
the-chicken-leg <michael.k.koch3@gmail.com>
2024-12-18 09:57:21 +0100

    add bfs

---
+++ bfs/main.py
@@ -0,0 +1,19 @@
+from graphics import Window
+from bfs import BFS
+
+def main():
+    # tuneables
+    window_width = 800
+    window_height = 600
+    cell_size = 50
+    start_i = 0
+    start_j = 0
+    sleep_time = 0.1
+    seed = None
+
+    window = Window(window_width, window_height)
+    cell_matrix = BFS(window, cell_size)
+    cell_matrix.search(start_i, start_j, sleep_time, seed)
+    window.wait_for_close()
+
+main()
