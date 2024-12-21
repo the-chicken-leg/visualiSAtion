5d735fa5de6316f751594d3d313ee5d2cdb180be
the-chicken-leg <michael.k.koch3@gmail.com>
2024-12-21 15:08:08 +0100

    modify defaults

--- queue/main.py
+++ queue/main.py
@@ -10,12 +10,13 @@ def main():
     window_height = 800
     starting_num_patrons = 15
     patron_fatbody_index = 25
-    push_pop_ratio = 0.75
+    push_pop_ratio = 0.5
     sleep_time = 0.3
     stop_when_empty = True
 
     window = Window(window_width, window_height)
     snackshack_queue = SnackshackQueue(window, starting_num_patrons, patron_fatbody_index)
+    input("Press Enter to continue")
     snackshack_queue.simulate(push_pop_ratio, sleep_time, stop_when_empty)
     window.wait_for_close()
 
