a8bebd0e25c05e55e0086ff7fb6b6f6cfcd64366
the-chicken-leg <michael.koch3@pm.me>
2026-04-06 09:39:12 +0200

    before I break something

---
+++ breadth_first_search/__init__.py
@@ -0,0 +1,3 @@
+from .breadth_first_search_main import run_breadth_first_search
+
+__all__ = ["run_breadth_first_search"]
--- visualiSAtion.py
+++ visualiSAtion.py
@@ -1,6 +1,6 @@
 import argparse
 
-from breadth_first_search.breadth_first_search_main import run_breadth_first_search
+from breadth_first_search import run_breadth_first_search
 from bubble_sort.bubble_sort_main import run_bubble_sort
 from depth_first_search.depth_first_search_main import run_depth_first_search
 from insertion_sort.insertion_sort_main import run_insertion_sort
@@ -11,33 +11,29 @@ from red_black_tree.red_black_tree_main import run_red_black_tree
 from selection_sort.selection_sort_main import run_selection_sort
 from stack.stack_main import run_stack
 
-def run_vis():
-    DISPATCH = {
-        "breadth_first_search": run_breadth_first_search,
-        "bubble_sort": run_bubble_sort,
-        "depth_first_search": run_depth_first_search,
-        "insertion_sort": run_insertion_sort,
-        "merge_sort": run_merge_sort,
-        "queue": run_queue,
-        "quick_sort": run_quick_sort,
-        "red_black_tree": run_red_black_tree,
-        "selection_sort": run_selection_sort,
-        "stack": run_stack,
-    }
+DISPATCH = {
+    "breadth_first_search": run_breadth_first_search,
+    "bubble_sort": run_bubble_sort,
+    "depth_first_search": run_depth_first_search,
+    "insertion_sort": run_insertion_sort,
+    "merge_sort": run_merge_sort,
+    "queue": run_queue,
+    "quick_sort": run_quick_sort,
+    "red_black_tree": run_red_black_tree,
+    "selection_sort": run_selection_sort,
+    "stack": run_stack,
+}
 
-    parser = argparse.ArgumentParser(
-        prog="visualiSAtion.py",
-        description="visualiSAtion of (data) Structures and Algorithms",
-    )
-    parser.add_argument(
-        "dsa",
-        choices=DISPATCH.keys(),
-        help="data structure or algorithm to run"
-    )
-    args = parser.parse_args()
+parser = argparse.ArgumentParser(
+    prog="visualiSAtion.py",
+    description="visualiSAtion of (data) Structures and Algorithms",
+)
+parser.add_argument(
+    "dsa",
+    choices=DISPATCH.keys(),
+    help="data structure or algorithm to run"
+)
+args = parser.parse_args()
 
-    handler = DISPATCH[args.dsa]
-    handler()
-
-if __name__ == "__main__":
-    run_vis()
+handler = DISPATCH[args.dsa]
+handler()
