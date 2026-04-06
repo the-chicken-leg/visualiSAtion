ef3624f190cb0e991ea15bc9ecce14ac0d83d7e2
the-chicken-leg <michael.koch3@pm.me>
2026-04-06 16:30:09 +0200

    merge_sort

---
+++ merge_sort/__init__.py
@@ -0,0 +1,3 @@
+from .merge_sort_main import run_merge_sort
+
+__all__ = ["run_merge_sort"]
--- visualiSAtion.py
+++ visualiSAtion.py
@@ -4,7 +4,7 @@ from breadth_first_search import run_breadth_first_search
 from bubble_sort import run_bubble_sort
 from depth_first_search import run_depth_first_search
 from insertion_sort import run_insertion_sort
-from merge_sort.merge_sort_main import run_merge_sort
+from merge_sort import run_merge_sort
 from queue.queue_main import run_queue
 from quick_sort.quick_sort_main import run_quick_sort
 from red_black_tree.red_black_tree_main import run_red_black_tree
@@ -31,7 +31,7 @@ parser = argparse.ArgumentParser(
 parser.add_argument(
     "dsa",
     choices=DISPATCH.keys(),
-    help="data structure or algorithm to run"
+    help="data structure or algorithm to run",
 )
 args = parser.parse_args()
 
